ddc849b46d876becca3a7e563e316a1af7eb3ef7
Eva Navarro <emanao@me.com>
2019-11-11 20:11:44 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,15 +6,8 @@ class Anagram
     @word=word
   end
   def match (anagram_array)
-    result= []
-    word_chars = word.strip.split(//).sort
-    # anagram_array.each do |anagram|
-    #   anagram_chars = anagram.strip.split(//).sort
-    #   result <<anagram if word_chars == anagram_chars
-    # end
     anagram_array.select {|anagram|
-      anagram.strip.split(//).sort == word_chars
+      anagram.strip.split(//).sort == word.strip.split(//).sort
     }
-    #result
   end
 end
